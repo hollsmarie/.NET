308d4686f82888e687121d79a0144c5e472a317f
Holly Valenty <holly914@gmail.com>
2018-03-05 18:47:17 -0600

    C# with different types

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> emptyList = new List<object>();
+            emptyList.Add(7);
+            emptyList.Add(28);
+            emptyList.Add(-1);
+            emptyList.Add(true);
+            emptyList.Add("chair");
+            int sum = new int();
+            
+            foreach (var entry in emptyList) {
+                Console.WriteLine(entry);
+                if(entry is int) {
+                    sum += (int)entry;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
