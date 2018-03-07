0ba0dd7b1fa8648b1f70956a8461bf529a3289e7
Holly Valenty <holly914@gmail.com>
2018-03-07 17:20:53 -0600

    A stab at the Coding Dojo Deck of Cards using C#

--- DeckOfCards/Deck.cs
+++ DeckOfCards/Deck.cs
@@ -6,38 +6,62 @@ namespace DeckOfCards
     {
         public List<Card> cards = new List<Card>(); //making a list of the card makes it refer to the object Card
 
-        public Deck ()
+        public Deck()
         {
-        string[] suits = {"Diamonds", "Hearts", "Clubs", "Spades"};
-        string[] stringVals = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+            reset();
+            shuffle();
+        }   
 
-        foreach(string suit in suits)
+        public Deck reset()
         {
-            for(int i =0; i <stringVals.Length; i++)
+
+            string[] suits = { "Diamonds", "Hearts", "Clubs", "Spades" };
+            string[] stringVals = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+            foreach (string suit in suits)
             {
-                Card newCard = new Card(suit, stringVals[i], i+1)
-                cards.Add(newCard);
+                for (int i = 0; i < stringVals.Length; i++)
+                {
+                    Card newCard = new Card(suit, stringVals[i], i + 1);
+                    cards.Add(newCard);
+                }
             }
+            return this;
         }
+            public Deck deal()
+            {
+                if(cards.Count > 0) 
+                {
+                    Card first = cards[0];
 
-        }
+                    cards.RemoveAt(0);
 
-    }
+                    return first;
+                } else {
+                    reset();
+                    return deal();
+                }
+            
+            }
 
-    public Deck deal()
-    {
 
-    }
+            public void  shuffle()
+            {
+                //iterate backwards through deck
+                Random rand = new Random();
+                for(int end = cards.Count-1; end >= 0; end --)
+                {
+                //assigns temp to shuffle variable, a random card in the deck    
+                int shuffle = rand.Next(0, cards.Count-1);
+                Card temp = cards[shuffle];
+                //swap without end value
+                cards[shuffle] = cards [end];
+                cards [end] = temp;
+                } 
+                
+            }
 
-    public Deck reset()
-    {
         
-    }
-
-    public Deck shuffle()
-    {
-        
-    }
 
     }
 }    
--- DeckOfCards/Player.cs
+++ DeckOfCards/Player.cs
@@ -4,6 +4,32 @@ namespace DeckOfCards
 {
     public class Player
     {
-    
+        string name; //Give the player class a name property
+        public List<Card> hand; // Give the player a hand property that is a List of type Card
+
+        public Player(string person)
+        {
+            name = person;
+            hand = new List<Card>();
+        }
+
+        public Card draw (Deck addhand) //Give the player a draw method this needs to reference the Deck object
+        {
+            Card newCard = addhand.deal();
+            hand.Add(newCard);
+            return newCard;
+        }
+
+        public Card discard(int idx)
+        {
+            if (idx < 0 || idx > hand.Count) {
+                System.Console.WriteLine("Discard dis");
+                return null;
+            } else {
+                Card shuffle = hand[idx];
+                hand.RemoveAt(idx);
+                return shuffle;
+            }
+        }
     }
 }    
